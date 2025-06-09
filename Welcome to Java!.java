223ead90d5d3cd8a48c098a0cd0a3c4f3af03de1
Ganesh Mane <ganeshashokmane95@gmail.com>
2025-06-09 11:36:29 +0530

    Update Welcome to Java!.java

--- Welcome to Java!.java
+++ Welcome to Java!.java
@@ -1,16 +1,3 @@
-/*Welcome to the world of Java! In this challenge, we practice printing to stdout.
-The code stubs in your editor declare a Solution class and a main method. Complete the main method by copying the two lines of code below and pasting them inside the body of your main method.
-  
-Input Format
-There is no input for this challenge.
-
-Output Format
-You must print two lines of output:
-1)Print Hello, World. on the first line.
-2)Print Hello, Java. on the second line.
-  */
-
-//Code
 
 public class Solution {
 
